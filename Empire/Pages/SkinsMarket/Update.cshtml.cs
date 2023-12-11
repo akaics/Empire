e0c127273c3fb84db7a87ff3f186c2adcf0fe5bb
Gülsüm Erdogan <guls0489@edu.zealand.dk>
2023-12-11 21:53:54 +0100

    feat: implemented an Update button on the SkinMarket site

---
+++ Empire/Pages/SkinsMarket/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Empire.Pages.SkinsMarket
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
