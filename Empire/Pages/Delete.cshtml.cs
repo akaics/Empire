263f82e4da204c65bff3ab5efbb0296babf4d6c5
Gülsüm Erdogan <guls0489@edu.zealand.dk>
2023-12-14 15:12:15 +0100

    deleted again

---
+++ Empire/Pages/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Empire.Data;
+using Empire.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Empire.Pages
+{
+    public class DeleteModel : PageModel
+    {
+
+    }
+}
