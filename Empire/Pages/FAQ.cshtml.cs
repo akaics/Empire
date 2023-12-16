f476517f439751f82170f0cf0d574cc3ff0004fe
Gülsüm Erdogan <guls0489@edu.zealand.dk>
2023-12-16 21:21:16 +0100

    faq side

---
+++ Empire/Pages/FAQ.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Empire.Pages
+{
+    public class FAQModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
