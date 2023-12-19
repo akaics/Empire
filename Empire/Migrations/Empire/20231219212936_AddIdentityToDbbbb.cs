ca54bae9755a167761ea3068a8aeb0e955eb72ac
Gülsüm Erdogan <guls0489@edu.zealand.dk>
2023-12-19 22:32:00 +0100

    feat: Scaffolded identity

---
+++ Empire/Migrations/Empire/20231219212936_AddIdentityToDbbbb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Empire.Migrations.Empire
+{
+    /// <inheritdoc />
+    public partial class AddIdentityToDbbbb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
